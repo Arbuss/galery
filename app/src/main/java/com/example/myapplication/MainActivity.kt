c66c4d871e2ce80576be83825d569790c086748d
Arbuss <artur.buss@bytepace.com>
2019-07-11 17:36:58 +0600

    17:35 grid view: ok nice interface: not ok photo collection: not ok

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,28 +3,25 @@ package com.example.myapplication
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import androidx.annotation.RequiresApi
 import java.io.File
 import android.Manifest.permission
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import androidx.core.net.toUri
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-
 class MainActivity : AppCompatActivity() {
-
-    lateinit var imageViewsList : List<ImageView>
-    lateinit var filesList : ArrayList<File>
-    lateinit var recyclerView : RecyclerView
-
-    fun printShit(): String {
-        return (0..100).random().toString()
-    }
+    lateinit var filesList: ArrayList<File>
+    lateinit var recyclerView: RecyclerView
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,42 +30,41 @@ class MainActivity : AppCompatActivity() {
         requestPermissions(arrayOf(permission.READ_EXTERNAL_STORAGE), 0)
         filesList = ArrayList()
 
-        imageViewsList = listOf(
-            findViewById(R.id.imageView),
-            findViewById(R.id.imageView2),
-            findViewById(R.id.imageView3),
-            findViewById(R.id.imageView4),
-            findViewById(R.id.imageView5),
-            findViewById(R.id.imageView6)
-        )
-
         recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = Adapter(imageViewsList)
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        /*if (ContextCompat.checkSelfPermission(this, permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "Permission is granted", Toast.LENGTH_LONG).show()
-        }*/
-        val rootFolder = Environment.getRootDirectory()
-        getListFiles(File("storage/emulated/0/DCIM/Camera"))
+        //getListFiles(File("storage/emulated/0/DCIM/Camera"))
+        recyclerView.adapter = Adapter(filesList)
 
-        if(filesList.isNotEmpty()) {
-            var i = 0
-            for(file: File in filesList){
-                if(i == 6)
-                    i = 0
-                imageViewsList[i].setImageURI(file.toUri())
-                i++
+        //val photoPickerIntent = Intent(Intent.ACTION_PICK)
+        //photoPickerIntent.type = "image/*"
+        //startActivityForResult(photoPickerIntent, 1)
+    }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, imageReturnedIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent)
+
+        //val bitmap : Bitmap = findViewById(R.id.imageView1)
+        val shittyImageView = findViewById<ImageView>(R.id.imageView1)
+        when (requestCode){
+            1 -> {
+                if(resultCode == RESULT_OK){
+                    val selectedImage = imageReturnedIntent?.data
+                    shittyImageView.setImageURI(selectedImage)
+                    //bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
+                    //shittyImageView.setImageBitmap(MediaStore.Images.Media.getBitmap(contentResolver, selectedImage))
+                }
             }
         }
-    }
+    }*/
 
     fun getListFiles(parentDir: File) {
         val files = parentDir.listFiles()
-        if(files != null) {
+        if (files != null) {
             for (file in files) {
                 if (file.isFile) {
                     filesList.add(file)
@@ -76,24 +72,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-}
 
-class Adapter(private val values: List<ImageView>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_view, parent, false)
-        return ViewHolder(itemView)
+    class Adapter(private val values: List<File>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_view, parent, false)
+            return ViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.imageView1?.setImageURI(values[position].toUri())
+        }
+
+        override fun getItemCount() = values.size
+
+        class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+            var imageView1: ImageView? = null
+
+            init {
+                imageView1 = itemView?.findViewById(R.id.imageView1)
+            }
+        }
     }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView = values[position]
-    }
-
-    override fun getItemCount() = values.size
-
-class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!){
-    var textView: ImageView? = null
-    init{
-        textView = itemView?.findViewById(R.id.imageView7)
-    }
-}
 }
